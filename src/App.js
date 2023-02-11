69f874dab04b78f7426fbe97584bbd3b6162aa26
chur96 <chanhurcaw@gmail.com>
2023-02-11 14:45:09 -0800

    exercises

--- src/App.js
+++ src/App.js
@@ -18,10 +18,18 @@ const useField = (type) => {
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  // ...
+  useEffect(() => {
+    axios
+      .get(baseUrl)
+      .then(response => {
+        setResources(response.data)
+      })
+  }, [setResources, baseUrl])
 
-  const create = (resource) => {
-    // ...
+  const create = async (resource) => {
+    const response = await axios.post(baseUrl, resource)
+    setResources(resources.concat(response.data))
+    return response.data
   }
 
   const service = {
